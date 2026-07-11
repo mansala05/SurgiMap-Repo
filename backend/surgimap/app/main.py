a8e803c376a0c90054cb00b4d5e27e5a0cbb3cfb
Sesanya <sesab1020@gmail.com>
2026-07-11 17:25:27 +0530

    Add sync endpoints and fix imports

--- backend/surgimap/app/main.py
+++ backend/surgimap/app/main.py
@@ -5,6 +5,7 @@
 from fastapi import FastAPI
 from app.database import engine, Base
 from app.routers import pharmacies, search,stock,sync
+from fastapi.middleware.cors import CORSMiddleware
 
 
 # Create all tables in PostgreSQL if they don't already exist
@@ -17,12 +18,19 @@ app = FastAPI(
     version="1.0.0",
 )
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = ['http://localhost:5173'],  #pabarusiri's React dev server
+    allow_credentials = True,
+    allow_methods = ['*'],
+    allow_headers = ['*'],
+)
+
 # Register routers — this is how FastAPI knows about your endpoints
 app.include_router(pharmacies.router)
 app.include_router(search.router)
 app.include_router(stock.router)
 app.include_router(sync.router)
-
 
 @app.get("/")
 def root():
